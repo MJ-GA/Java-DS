786312c86c72e0436bef8a0ef40cc779a8540abe
Mohamed Jouini <joui0005@algonquinlive.com>
2023-03-04 20:32:50 -0500

    .

--- Lab6/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"FullTimeStudent"},{"p":"<Unnamed>","l":"PartTimeStudent"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"Student"},{"p":"<Unnamed>","l":"University"},{"p":"<Unnamed>","l":"UniversitySystemTest"}];updateSearchResults();
